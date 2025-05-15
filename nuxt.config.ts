87b10855dd5b46d0a3e2e75ff04590555755f185
NickRisinger <boozy044@gmail.com>
2025-05-15 18:19:24 +0300

    update rules route for serverless

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  routeRules: {
+    '/api/**': {
+      proxy: process.env.NODE_ENV === "development" ? "http://127.0.0.1:8000/api/**" : "/api/**",
+    },
+    '/docs': {
+      proxy: "http://127.0.0.1:8000/docs",
+    },
+    '/openapi.json': {
+      proxy: "http://127.0.0.1:8000/openapi.json",
+    }
+  },
+
   nitro: {
     preset: "vercel",
-
-    routeRules: {
-      '/api/**': { proxy: '/api/:path*', cors: true },
-    },
   },
 })
