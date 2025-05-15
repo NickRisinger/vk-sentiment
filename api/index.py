d21e26c6ef1a63b7cf8a3d57ce38716e40267cdd
NickRisinger <boozy044@gmail.com>
2025-05-15 19:15:28 +0300

    add auth api

--- api/index.py
+++ api/index.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+@app.get("/api/auth")
+async def handle_auth(payload: Optional[str] = None):
+    data = json.loads(payload)
+    return data
 
 @app.get("/api/hello")
 async def handle_chat_data():
